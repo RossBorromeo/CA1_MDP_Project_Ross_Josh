0799f9100dcfd61b2012d3d5c319355747afe6ce
RossBorromeo <91060165+RossBorromeo@users.noreply.github.com>
2025-01-12 15:23:58 +0000

    First Commit

---
+++ CA1_MDP_Project/Application.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "ResourceHolder.hpp"
+#include "ResourceIdentifiers.hpp"
+#include "StateStack.hpp"
+#include "MusicPlayer.hpp"
+#include "SoundPlayer.hpp"
+
+class Application
+{
+public:
+	Application();
+	void Run();
+
+private:
+	void ProcessInput();
+	void Update(sf::Time dt);
+	void Render();
+	void RegisterStates();
+
+private:
+	TextureHolder m_textures;
+	FontHolder m_fonts;
+
+	StateStack m_stack;
+	static const sf::Time kTimePerFrame;
+
+	MusicPlayer m_music;
+	SoundPlayer m_sound;
+};
+
---
+++ CA1_MDP_Project/Main.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+#include "ResourceIdentifiers.hpp"
+#include <iostream>
+#include "Application.hpp"
+
+int main()
+{
+	//TextureHolder game_textures;
+	try
+	{
+		Application app;
+		app.Run();
+	}
+	catch (std::runtime_error& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
