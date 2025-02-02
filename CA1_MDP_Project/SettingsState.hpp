b4542f77b0b62a5c69766d4af05aef13fff9e219
XxJT420xX <D00238448@student.dkit.ie>
2025-02-02 23:22:12 +0000

    Settings State restored but is only available for Player 1 working to fix for Player 2

--- CA1_MDP_Project/SettingsState.hpp
+++ CA1_MDP_Project/SettingsState.hpp
@@ -19,7 +19,7 @@ public:
 
 private:
 	void UpdateLabels();
-	void AddButtonLabel(Action action, float y, const std::string& text, Context context);
+	void AddButtonLabel(Action action, float y, const std::string& text, Context context, bool isPlayer1);
 
 private:
 	sf::Sprite m_background_sprite;
